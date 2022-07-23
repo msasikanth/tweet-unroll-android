c0e891a103699de690c53828624b4e46c7bdd4ca
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-23 15:33:34 +0530

    Add KSP plugin dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
   alias(libs.plugins.android.library).apply(false)
   alias(libs.plugins.kotlin.android).apply(false)
   alias(libs.plugins.hilt).apply(false)
+  alias(libs.plugins.ksp).apply(false)
 }
 
 tasks.register("clean") {
