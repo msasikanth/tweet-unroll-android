c744985efb919adfa1c80b1fb29ad18ca9af3054
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-23 14:33:25 +0530

    Implement `TwineAuthInterceptor`

---
+++ auth/src/main/java/dev/sasikanth/twine/auth/TwineAuthInterceptor.kt
@@ -0,0 +1,37 @@
+package dev.sasikanth.twine.auth
+
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import javax.inject.Inject
+
+/**
+ * Class to intercept OkHttps [Request]'s and add `Authorization` header
+ * if there is an access token
+ *
+ * @param authManager: [AuthManager] is used to fetch the fresh access token
+ *
+ */
+class TwineAuthInterceptor @Inject constructor(
+  private val authManager: AuthManager
+) : Interceptor {
+
+  override fun intercept(chain: Interceptor.Chain): Response {
+    val request = chain.request()
+    return chain.proceed(addAuthorizationHeader(request))
+  }
+
+  private fun addAuthorizationHeader(request: Request): Request {
+    val accessToken = runBlocking { authManager.fetchFreshAccessToken() }
+
+    return if (accessToken != null) {
+      request
+        .newBuilder()
+        .header("Authorization", "Bearer $accessToken")
+        .build()
+    } else {
+      request
+    }
+  }
+}
