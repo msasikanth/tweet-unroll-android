c7dcb470310779ffedd8c342b874400ee9ab0518
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-23 19:16:49 +0530

    Add common-testing module

---
+++ common-testing/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+  id("com.android.library")
+  id("org.jetbrains.kotlin.android")
+  kotlin("kapt")
+  id("dagger.hilt.android.plugin")
+}
+
+android {
+  namespace = "dev.sasikanth.twine.common.testing"
+  compileSdk = libs.versions.sdk.compile.get().toInt()
+
+  defaultConfig {
+    minSdk = libs.versions.sdk.min.get().toInt()
+    targetSdk = libs.versions.sdk.target.get().toInt()
+
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    consumerProguardFiles("consumer-rules.pro")
+  }
+
+  buildTypes {
+    release {
+      isMinifyEnabled = false
+      proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+    }
+  }
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+  }
+  kotlinOptions {
+    jvmTarget = "1.8"
+  }
+}
+
+kapt {
+  correctErrorTypes = true
+}
+
+dependencies {
+  implementation(libs.androidx.core)
+  implementation(libs.hilt)
+  kapt(libs.hilt.compiler)
+
+  api(libs.junit)
+  api(libs.androidx.test.junit)
+  api(libs.androidx.test.espresso)
+  api(libs.hilt.test)
+}
