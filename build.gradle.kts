25ae097f3d729d848c570b797c82330ee2831f6c
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-24 15:22:49 +0530

    Suppress DSL scope violation in root build.gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+@Suppress("DSL_SCOPE_VIOLATION")
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
   alias(libs.plugins.android.application).apply(false)
