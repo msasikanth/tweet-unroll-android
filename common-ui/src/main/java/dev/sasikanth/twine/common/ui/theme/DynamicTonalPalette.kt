8a533f9c817ff54eb4bc075e0f7f4fc7f2f45fb9
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-31 01:13:40 +0530

    Add `TwineColorScheme`

---
+++ common-ui/src/main/java/dev/sasikanth/twine/common/ui/theme/DynamicTonalPalette.kt
@@ -0,0 +1,79 @@
+package dev.sasikanth.twine.common.ui.theme
+
+import android.content.Context
+import android.os.Build
+import androidx.annotation.ColorRes
+import androidx.annotation.DoNotInline
+import androidx.annotation.RequiresApi
+import androidx.compose.ui.graphics.Color
+import androidx.compose.material3.dynamicDarkColorScheme as materialDynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme as materialDynamicLightColorScheme
+
+@RequiresApi(Build.VERSION_CODES.S)
+fun dynamicLightColorScheme(context: Context): TwineColorScheme {
+  val materialDynamicLightColorScheme = materialDynamicLightColorScheme(context)
+  return lightColorScheme(
+    brand = ColorResourceHelper.getColor(context, android.R.color.system_accent1_200),
+    onBrand = ColorResourceHelper.getColor(context, android.R.color.system_accent1_800),
+    primary = materialDynamicLightColorScheme.primary,
+    onPrimary = materialDynamicLightColorScheme.onPrimary,
+    primaryContainer = materialDynamicLightColorScheme.primaryContainer,
+    onPrimaryContainer = materialDynamicLightColorScheme.onPrimaryContainer,
+    secondary = materialDynamicLightColorScheme.secondary,
+    onSecondary = materialDynamicLightColorScheme.onSecondary,
+    secondaryContainer = materialDynamicLightColorScheme.secondaryContainer,
+    onSecondaryContainer = materialDynamicLightColorScheme.onSecondaryContainer,
+    tertiary = materialDynamicLightColorScheme.tertiary,
+    onTertiary = materialDynamicLightColorScheme.onTertiary,
+    tertiaryContainer = materialDynamicLightColorScheme.tertiaryContainer,
+    onTertiaryContainer = materialDynamicLightColorScheme.onTertiaryContainer,
+    background = materialDynamicLightColorScheme.background,
+    onBackground = materialDynamicLightColorScheme.onBackground,
+    surface = materialDynamicLightColorScheme.surface,
+    onSurface = materialDynamicLightColorScheme.onSurface,
+    surfaceVariant = materialDynamicLightColorScheme.surfaceVariant,
+    onSurfaceVariant = materialDynamicLightColorScheme.onSurfaceVariant,
+    outline = materialDynamicLightColorScheme.outline,
+    inverseOnSurface = materialDynamicLightColorScheme.inverseOnSurface,
+    inverseSurface = materialDynamicLightColorScheme.inverseSurface,
+    inversePrimary = materialDynamicLightColorScheme.inversePrimary,
+  )
+}
+
+@RequiresApi(Build.VERSION_CODES.S)
+fun dynamicDarkColorScheme(context: Context): TwineColorScheme {
+  val materialDynamicDarkColorScheme = materialDynamicDarkColorScheme(context)
+  return darkColorScheme(
+    brand = ColorResourceHelper.getColor(context, android.R.color.system_accent1_200),
+    onBrand = ColorResourceHelper.getColor(context, android.R.color.system_accent1_800),
+    primary = materialDynamicDarkColorScheme.primary,
+    onPrimary = materialDynamicDarkColorScheme.onPrimary,
+    primaryContainer = materialDynamicDarkColorScheme.primaryContainer,
+    onPrimaryContainer = materialDynamicDarkColorScheme.onPrimaryContainer,
+    secondary = materialDynamicDarkColorScheme.secondary,
+    onSecondary = materialDynamicDarkColorScheme.onSecondary,
+    secondaryContainer = materialDynamicDarkColorScheme.secondaryContainer,
+    onSecondaryContainer = materialDynamicDarkColorScheme.onSecondaryContainer,
+    tertiary = materialDynamicDarkColorScheme.tertiary,
+    onTertiary = materialDynamicDarkColorScheme.onTertiary,
+    tertiaryContainer = materialDynamicDarkColorScheme.tertiaryContainer,
+    onTertiaryContainer = materialDynamicDarkColorScheme.onTertiaryContainer,
+    background = materialDynamicDarkColorScheme.background,
+    onBackground = materialDynamicDarkColorScheme.onBackground,
+    surface = materialDynamicDarkColorScheme.surface,
+    onSurface = materialDynamicDarkColorScheme.onSurface,
+    surfaceVariant = materialDynamicDarkColorScheme.surfaceVariant,
+    onSurfaceVariant = materialDynamicDarkColorScheme.onSurfaceVariant,
+    outline = materialDynamicDarkColorScheme.outline,
+    inverseOnSurface = materialDynamicDarkColorScheme.inverseOnSurface,
+    inverseSurface = materialDynamicDarkColorScheme.inverseSurface,
+    inversePrimary = materialDynamicDarkColorScheme.inversePrimary,
+  )
+}
+
+private object ColorResourceHelper {
+  @DoNotInline
+  fun getColor(context: Context, @ColorRes id: Int): Color {
+    return Color(context.resources.getColor(id, context.theme))
+  }
+}
---
+++ common-ui/src/main/java/dev/sasikanth/twine/common/ui/theme/TwineColorScheme.kt
@@ -0,0 +1,350 @@
+package dev.sasikanth.twine.common.ui.theme
+
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.runtime.structuralEqualityPolicy
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.takeOrElse
+import androidx.compose.material3.ColorScheme as MaterialColorScheme
+
+@Immutable
+class TwineColorScheme(
+  brand: Color,
+  onBrand: Color,
+  primary: Color,
+  onPrimary: Color,
+  primaryContainer: Color,
+  onPrimaryContainer: Color,
+  inversePrimary: Color,
+  secondary: Color,
+  onSecondary: Color,
+  secondaryContainer: Color,
+  onSecondaryContainer: Color,
+  tertiary: Color,
+  onTertiary: Color,
+  tertiaryContainer: Color,
+  onTertiaryContainer: Color,
+  background: Color,
+  onBackground: Color,
+  surface: Color,
+  onSurface: Color,
+  surfaceVariant: Color,
+  onSurfaceVariant: Color,
+  surfaceTint: Color,
+  inverseSurface: Color,
+  inverseOnSurface: Color,
+  error: Color,
+  onError: Color,
+  errorContainer: Color,
+  onErrorContainer: Color,
+  outline: Color
+) {
+  var brand by mutableStateOf(brand, structuralEqualityPolicy())
+    internal set
+  var onBrand by mutableStateOf(onBrand, structuralEqualityPolicy())
+    internal set
+  var primary by mutableStateOf(primary, structuralEqualityPolicy())
+    internal set
+  var onPrimary by mutableStateOf(onPrimary, structuralEqualityPolicy())
+    internal set
+  var primaryContainer by mutableStateOf(primaryContainer, structuralEqualityPolicy())
+    internal set
+  var onPrimaryContainer by mutableStateOf(onPrimaryContainer, structuralEqualityPolicy())
+    internal set
+  var inversePrimary by mutableStateOf(inversePrimary, structuralEqualityPolicy())
+    internal set
+  var secondary by mutableStateOf(secondary, structuralEqualityPolicy())
+    internal set
+  var onSecondary by mutableStateOf(onSecondary, structuralEqualityPolicy())
+    internal set
+  var secondaryContainer by mutableStateOf(secondaryContainer, structuralEqualityPolicy())
+    internal set
+  var onSecondaryContainer by mutableStateOf(onSecondaryContainer, structuralEqualityPolicy())
+    internal set
+  var tertiary by mutableStateOf(tertiary, structuralEqualityPolicy())
+    internal set
+  var onTertiary by mutableStateOf(onTertiary, structuralEqualityPolicy())
+    internal set
+  var tertiaryContainer by mutableStateOf(tertiaryContainer, structuralEqualityPolicy())
+    internal set
+  var onTertiaryContainer by mutableStateOf(onTertiaryContainer, structuralEqualityPolicy())
+    internal set
+  var background by mutableStateOf(background, structuralEqualityPolicy())
+    internal set
+  var onBackground by mutableStateOf(onBackground, structuralEqualityPolicy())
+    internal set
+  var surface by mutableStateOf(surface, structuralEqualityPolicy())
+    internal set
+  var onSurface by mutableStateOf(onSurface, structuralEqualityPolicy())
+    internal set
+  var surfaceVariant by mutableStateOf(surfaceVariant, structuralEqualityPolicy())
+    internal set
+  var onSurfaceVariant by mutableStateOf(onSurfaceVariant, structuralEqualityPolicy())
+    internal set
+  var surfaceTint by mutableStateOf(surfaceTint, structuralEqualityPolicy())
+    internal set
+  var inverseSurface by mutableStateOf(inverseSurface, structuralEqualityPolicy())
+    internal set
+  var inverseOnSurface by mutableStateOf(inverseOnSurface, structuralEqualityPolicy())
+    internal set
+  var error by mutableStateOf(error, structuralEqualityPolicy())
+    internal set
+  var onError by mutableStateOf(onError, structuralEqualityPolicy())
+    internal set
+  var errorContainer by mutableStateOf(errorContainer, structuralEqualityPolicy())
+    internal set
+  var onErrorContainer by mutableStateOf(onErrorContainer, structuralEqualityPolicy())
+    internal set
+  var outline by mutableStateOf(outline, structuralEqualityPolicy())
+    internal set
+
+  fun copy(
+    brand: Color = this.brand,
+    onBrand: Color = this.onBrand,
+    primary: Color = this.primary,
+    onPrimary: Color = this.onPrimary,
+    primaryContainer: Color = this.primaryContainer,
+    onPrimaryContainer: Color = this.onPrimaryContainer,
+    inversePrimary: Color = this.inversePrimary,
+    secondary: Color = this.secondary,
+    onSecondary: Color = this.onSecondary,
+    secondaryContainer: Color = this.secondaryContainer,
+    onSecondaryContainer: Color = this.onSecondaryContainer,
+    tertiary: Color = this.tertiary,
+    onTertiary: Color = this.onTertiary,
+    tertiaryContainer: Color = this.tertiaryContainer,
+    onTertiaryContainer: Color = this.onTertiaryContainer,
+    background: Color = this.background,
+    onBackground: Color = this.onBackground,
+    surface: Color = this.surface,
+    onSurface: Color = this.onSurface,
+    surfaceVariant: Color = this.surfaceVariant,
+    onSurfaceVariant: Color = this.onSurfaceVariant,
+    surfaceTint: Color = this.surfaceTint,
+    inverseSurface: Color = this.inverseSurface,
+    inverseOnSurface: Color = this.inverseOnSurface,
+    error: Color = this.error,
+    onError: Color = this.onError,
+    errorContainer: Color = this.errorContainer,
+    onErrorContainer: Color = this.onErrorContainer,
+    outline: Color = this.outline
+  ): TwineColorScheme =
+    TwineColorScheme(
+      brand = brand,
+      onBrand = onBrand,
+      primary = primary,
+      onPrimary = onPrimary,
+      primaryContainer = primaryContainer,
+      onPrimaryContainer = onPrimaryContainer,
+      inversePrimary = inversePrimary,
+      secondary = secondary,
+      onSecondary = onSecondary,
+      secondaryContainer = secondaryContainer,
+      onSecondaryContainer = onSecondaryContainer,
+      tertiary = tertiary,
+      onTertiary = onTertiary,
+      tertiaryContainer = tertiaryContainer,
+      onTertiaryContainer = onTertiaryContainer,
+      background = background,
+      onBackground = onBackground,
+      surface = surface,
+      onSurface = onSurface,
+      surfaceVariant = surfaceVariant,
+      onSurfaceVariant = onSurfaceVariant,
+      surfaceTint = surfaceTint,
+      inverseSurface = inverseSurface,
+      inverseOnSurface = inverseOnSurface,
+      error = error,
+      onError = onError,
+      errorContainer = errorContainer,
+      onErrorContainer = onErrorContainer,
+      outline = outline
+    )
+
+  fun toMaterialColorScheme(): MaterialColorScheme =
+    MaterialColorScheme(
+      primary = primary,
+      onPrimary = onPrimary,
+      primaryContainer = primaryContainer,
+      onPrimaryContainer = onPrimaryContainer,
+      inversePrimary = inversePrimary,
+      secondary = secondary,
+      onSecondary = onSecondary,
+      secondaryContainer = secondaryContainer,
+      onSecondaryContainer = onSecondaryContainer,
+      tertiary = tertiary,
+      onTertiary = onTertiary,
+      tertiaryContainer = tertiaryContainer,
+      onTertiaryContainer = onTertiaryContainer,
+      background = background,
+      onBackground = onBackground,
+      surface = surface,
+      onSurface = onSurface,
+      surfaceVariant = surfaceVariant,
+      onSurfaceVariant = onSurfaceVariant,
+      surfaceTint = surfaceTint,
+      inverseSurface = inverseSurface,
+      inverseOnSurface = inverseOnSurface,
+      error = error,
+      onError = onError,
+      errorContainer = errorContainer,
+      onErrorContainer = onErrorContainer,
+      outline = outline
+    )
+}
+
+internal fun lightColorScheme(
+  brand: Color = md_theme_primary_80,
+  onBrand: Color = md_theme_primary_20,
+  primary: Color = md_theme_light_primary,
+  onPrimary: Color = md_theme_light_onPrimary,
+  primaryContainer: Color = md_theme_light_primaryContainer,
+  onPrimaryContainer: Color = md_theme_light_onPrimaryContainer,
+  secondary: Color = md_theme_light_secondary,
+  onSecondary: Color = md_theme_light_onSecondary,
+  secondaryContainer: Color = md_theme_light_secondaryContainer,
+  onSecondaryContainer: Color = md_theme_light_onSecondaryContainer,
+  tertiary: Color = md_theme_light_tertiary,
+  onTertiary: Color = md_theme_light_onTertiary,
+  tertiaryContainer: Color = md_theme_light_tertiaryContainer,
+  onTertiaryContainer: Color = md_theme_light_onTertiaryContainer,
+  error: Color = md_theme_light_error,
+  errorContainer: Color = md_theme_light_errorContainer,
+  onError: Color = md_theme_light_onError,
+  onErrorContainer: Color = md_theme_light_onErrorContainer,
+  background: Color = md_theme_light_background,
+  onBackground: Color = md_theme_light_onBackground,
+  surface: Color = md_theme_light_surface,
+  onSurface: Color = md_theme_light_onSurface,
+  surfaceVariant: Color = md_theme_light_surfaceVariant,
+  onSurfaceVariant: Color = md_theme_light_onSurfaceVariant,
+  outline: Color = md_theme_light_outline,
+  inverseOnSurface: Color = md_theme_light_inverseOnSurface,
+  inverseSurface: Color = md_theme_light_inverseSurface,
+  inversePrimary: Color = md_theme_light_inversePrimary,
+  surfaceTint: Color = md_theme_light_surfaceTint,
+): TwineColorScheme = TwineColorScheme(
+  brand = brand,
+  onBrand = onBrand,
+  primary = primary,
+  onPrimary = onPrimary,
+  primaryContainer = primaryContainer,
+  onPrimaryContainer = onPrimaryContainer,
+  inversePrimary = inversePrimary,
+  secondary = secondary,
+  onSecondary = onSecondary,
+  secondaryContainer = secondaryContainer,
+  onSecondaryContainer = onSecondaryContainer,
+  tertiary = tertiary,
+  onTertiary = onTertiary,
+  tertiaryContainer = tertiaryContainer,
+  onTertiaryContainer = onTertiaryContainer,
+  background = background,
+  onBackground = onBackground,
+  surface = surface,
+  onSurface = onSurface,
+  surfaceVariant = surfaceVariant,
+  onSurfaceVariant = onSurfaceVariant,
+  surfaceTint = surfaceTint,
+  inverseSurface = inverseSurface,
+  inverseOnSurface = inverseOnSurface,
+  error = error,
+  onError = onError,
+  errorContainer = errorContainer,
+  onErrorContainer = onErrorContainer,
+  outline = outline,
+)
+
+internal fun darkColorScheme(
+  brand: Color = md_theme_primary_80,
+  onBrand: Color = md_theme_primary_20,
+  primary: Color = md_theme_dark_primary,
+  onPrimary: Color = md_theme_dark_onPrimary,
+  primaryContainer: Color = md_theme_dark_primaryContainer,
+  onPrimaryContainer: Color = md_theme_dark_onPrimaryContainer,
+  secondary: Color = md_theme_dark_secondary,
+  onSecondary: Color = md_theme_dark_onSecondary,
+  secondaryContainer: Color = md_theme_dark_secondaryContainer,
+  onSecondaryContainer: Color = md_theme_dark_onSecondaryContainer,
+  tertiary: Color = md_theme_dark_tertiary,
+  onTertiary: Color = md_theme_dark_onTertiary,
+  tertiaryContainer: Color = md_theme_dark_tertiaryContainer,
+  onTertiaryContainer: Color = md_theme_dark_onTertiaryContainer,
+  error: Color = md_theme_dark_error,
+  errorContainer: Color = md_theme_dark_errorContainer,
+  onError: Color = md_theme_dark_onError,
+  onErrorContainer: Color = md_theme_dark_onErrorContainer,
+  background: Color = md_theme_dark_background,
+  onBackground: Color = md_theme_dark_onBackground,
+  surface: Color = md_theme_dark_surface,
+  onSurface: Color = md_theme_dark_onSurface,
+  surfaceVariant: Color = md_theme_dark_surfaceVariant,
+  onSurfaceVariant: Color = md_theme_dark_onSurfaceVariant,
+  outline: Color = md_theme_dark_outline,
+  inverseOnSurface: Color = md_theme_dark_inverseOnSurface,
+  inverseSurface: Color = md_theme_dark_inverseSurface,
+  inversePrimary: Color = md_theme_dark_inversePrimary,
+  surfaceTint: Color = md_theme_dark_surfaceTint,
+): TwineColorScheme = TwineColorScheme(
+  brand = brand,
+  onBrand = onBrand,
+  primary = primary,
+  onPrimary = onPrimary,
+  primaryContainer = primaryContainer,
+  onPrimaryContainer = onPrimaryContainer,
+  inversePrimary = inversePrimary,
+  secondary = secondary,
+  onSecondary = onSecondary,
+  secondaryContainer = secondaryContainer,
+  onSecondaryContainer = onSecondaryContainer,
+  tertiary = tertiary,
+  onTertiary = onTertiary,
+  tertiaryContainer = tertiaryContainer,
+  onTertiaryContainer = onTertiaryContainer,
+  background = background,
+  onBackground = onBackground,
+  surface = surface,
+  onSurface = onSurface,
+  surfaceVariant = surfaceVariant,
+  onSurfaceVariant = onSurfaceVariant,
+  surfaceTint = surfaceTint,
+  inverseSurface = inverseSurface,
+  inverseOnSurface = inverseOnSurface,
+  error = error,
+  onError = onError,
+  errorContainer = errorContainer,
+  onErrorContainer = onErrorContainer,
+  outline = outline,
+)
+
+fun TwineColorScheme.contentColorFor(backgroundColor: Color): Color = when (backgroundColor) {
+  primary -> onPrimary
+  brand -> onBrand
+  secondary -> onSecondary
+  tertiary -> onTertiary
+  background -> onBackground
+  error -> onError
+  surface -> onSurface
+  surfaceVariant -> onSurfaceVariant
+  primaryContainer -> onPrimaryContainer
+  secondaryContainer -> onSecondaryContainer
+  tertiaryContainer -> onTertiaryContainer
+  errorContainer -> onErrorContainer
+  inverseSurface -> inverseOnSurface
+  else -> Color.Unspecified
+}
+
+@Composable
+@ReadOnlyComposable
+fun contentColorFor(backgroundColor: Color) =
+  TwineTheme.colorScheme.contentColorFor(backgroundColor).takeOrElse {
+    LocalContentColor.current
+  }
+
+internal val LocalTwineColorScheme = staticCompositionLocalOf { lightColorScheme() }
