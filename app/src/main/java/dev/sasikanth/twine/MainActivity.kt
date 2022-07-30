db4ff9c5461d6752a2a4c7ad0fe43ca9af194afc
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-07-31 00:07:40 +0530

    Add `TwineTheme` in `common-ui` module

--- app/src/main/java/dev/sasikanth/twine/MainActivity.kt
+++ app/src/main/java/dev/sasikanth/twine/MainActivity.kt
@@ -4,25 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.sasikanth.twine.ui.theme.TwineTheme
+import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
+import dev.sasikanth.twine.common.ui.theme.TwineTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    WindowCompat.setDecorFitsSystemWindows(window, false)
     setContent {
       TwineTheme {
         // A surface container using the 'background' color from the theme
         Surface(
           modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colorScheme.background
+          color = TwineTheme.colorScheme.brand
         ) {
-          Greeting("Android")
+          Greeting(
+            modifier = Modifier.systemBarsPadding(),
+            name = "Android"
+          )
         }
       }
     }
@@ -30,14 +37,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Greeting(
+  modifier: Modifier = Modifier,
+  name: String
+) {
+  Text(
+    modifier = modifier
+      .padding(16.dp),
+    text = "Hello $name!",
+    style = TwineTheme.typography.titleMedium,
+    color = TwineTheme.colorScheme.onBrand
+  )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
   TwineTheme {
-    Greeting("Android")
+    Greeting(name = "Android")
   }
 }
---
+++ common-ui/src/main/java/dev/sasikanth/twine/common/ui/theme/Theme.kt
@@ -0,0 +1,62 @@
+package dev.sasikanth.twine.common.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Typography
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+@Composable
+fun TwineTheme(
+  useDarkTheme: Boolean = isSystemInDarkTheme(),
+  useDynamicColors: Boolean = true,
+  content: @Composable() () -> Unit
+) {
+  val colorScheme = when {
+    useDynamicColors && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+      val context = LocalContext.current
+
+      if (useDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+    }
+    useDarkTheme -> darkColorScheme()
+    else -> lightColorScheme()
+  }
+
+  val view = LocalView.current
+  if (!view.isInEditMode) {
+    SideEffect {
+      val window = (view.context as Activity).window
+      WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !useDarkTheme
+    }
+  }
+
+  CompositionLocalProvider(
+    LocalTwineColorScheme provides colorScheme
+  ) {
+    MaterialTheme(
+      colorScheme = TwineTheme.colorScheme.toMaterialColorScheme(),
+      typography = TwineTypography,
+      content = content
+    )
+  }
+}
+
+object TwineTheme {
+
+  val typography: Typography
+    @Composable
+    @ReadOnlyComposable
+    get() = MaterialTheme.typography
+
+  val colorScheme: TwineColorScheme
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalTwineColorScheme.current
+}
