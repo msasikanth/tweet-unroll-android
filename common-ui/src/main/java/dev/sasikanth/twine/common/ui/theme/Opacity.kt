e474b492b966c64d94402500b346e87b5353ddc9
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-08-05 07:41:59 +0530

    Add focused opacity token

--- common-ui/src/main/java/dev/sasikanth/twine/common/ui/theme/Opacity.kt
+++ common-ui/src/main/java/dev/sasikanth/twine/common/ui/theme/Opacity.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.staticCompositionLocalOf
 
 data class Opacity(
   val hovered: Float = 0.08f,
+  val focused: Float = 0.12f,
   val pressed: Float = 0.16f,
   val dragged: Float = 0.24f,
   val bgDisabled: Float = 0.12f,
