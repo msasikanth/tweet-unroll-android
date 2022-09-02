3145f810c70c3ab95b1ea3dd2b0cc1cd1d9139b1
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-09-02 09:38:35 +0530

    Add animation for alert toggle

--- ui-common/src/main/java/dev/sasikanth/twine/common/ui/components/AlertToggle.kt
+++ ui-common/src/main/java/dev/sasikanth/twine/common/ui/components/AlertToggle.kt
@@ -1,36 +1,53 @@
 package dev.sasikanth.twine.common.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.animation.graphics.ExperimentalAnimationGraphicsApi
 import androidx.compose.animation.graphics.res.animatedVectorResource
 import androidx.compose.animation.graphics.res.rememberAnimatedVectorPainter
 import androidx.compose.animation.graphics.vector.AnimatedImageVector
+import androidx.compose.animation.scaleIn
+import androidx.compose.animation.scaleOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.sasikanth.twine.common.ui.R
+import dev.sasikanth.twine.common.ui.anim.TwineSpring
+import dev.sasikanth.twine.common.ui.components.AlertToggleDefaults.InnerContainerSize
+import dev.sasikanth.twine.common.ui.components.AlertToggleDefaults.OuterContainerSize
+import dev.sasikanth.twine.common.ui.components.AlertToggleDefaults.ToggledContainerCornerRadius
+import dev.sasikanth.twine.common.ui.components.AlertToggleDefaults.UnToggledContainerCornerRadius
 import dev.sasikanth.twine.common.ui.theme.ElevationTokens
 import dev.sasikanth.twine.common.ui.theme.TwineTheme
 import dev.sasikanth.twine.common.ui.theme.surfaceColorAtElevation
 
 private object AlertToggleDefaults {
-  val containerCornerRadius = 20.dp
-  val containerPadding = 4.dp
-  val contentPadding = 16.dp
+  val ToggledContainerCornerRadius = 20.dp
+  val UnToggledContainerCornerRadius = 16.dp
+
+  val OuterContainerSize = 64.dp
+  val InnerContainerSize = 56.dp
 }
 
 /**
@@ -48,12 +65,6 @@ fun AlertToggle(
   isToggled: Boolean,
   onAlertToggled: (Boolean) -> Unit
 ) {
-  val containerColor = if (isToggled) {
-    TwineTheme.colorScheme.surfaceColorAtElevation(ElevationTokens.Level5)
-  } else {
-    Color.Unspecified
-  }
-
   val rippleColor = if (isToggled) {
     TwineTheme.colorScheme.onBrand
   } else {
@@ -61,53 +72,59 @@ fun AlertToggle(
   }
 
   Box(
-    modifier = modifier
-      .wrapContentHeight()
-      .wrapContentWidth()
-      .background(
-        color = containerColor,
-        shape = RoundedCornerShape(AlertToggleDefaults.containerCornerRadius)
-      )
-      .padding(AlertToggleDefaults.containerPadding)
-      .clip(TwineTheme.shapes.large)
-      .toggleable(
-        value = isToggled,
-        onValueChange = onAlertToggled,
-        interactionSource = remember { MutableInteractionSource() },
-        indication = rememberRipple(color = rippleColor)
-      ),
+    modifier = modifier.size(OuterContainerSize),
+    contentAlignment = Alignment.Center
   ) {
-    Content(isToggled = isToggled)
+    val containerColor = if (isToggled) {
+      TwineTheme.colorScheme.surfaceColorAtElevation(ElevationTokens.Level5)
+    } else {
+      TwineTheme
+        .colorScheme
+        .primary
+        .copy(
+          alpha = TwineTheme.opacity.hovered
+        )
+    }
+
+    val transition = updateTransition(targetState = isToggled, label = "AlertToggle")
+    val backgroundContainerSize by transition
+      .animateDp(label = "AlertToggle:BackgroundContainer") {
+        if (it) OuterContainerSize else InnerContainerSize
+      }
+
+    val cornerRadius by transition
+      .animateDp(label = "AlertToggled:CornerRadius") {
+        if (it) ToggledContainerCornerRadius else UnToggledContainerCornerRadius
+      }
+
+    Box(
+      modifier = Modifier
+        .clip(RoundedCornerShape(cornerRadius))
+        .toggleable(
+          value = isToggled,
+          onValueChange = onAlertToggled,
+          interactionSource = remember { MutableInteractionSource() },
+          indication = rememberRipple(color = rippleColor)
+        )
+        .size(backgroundContainerSize)
+        .background(color = containerColor),
+      contentAlignment = Alignment.Center
+    ) {
+      Content(isToggled = isToggled)
+    }
   }
 }
 
-@OptIn(ExperimentalAnimationGraphicsApi::class)
+@OptIn(ExperimentalAnimationGraphicsApi::class, ExperimentalAnimationApi::class)
 @Composable
 private fun Content(
   modifier: Modifier = Modifier,
   isToggled: Boolean
 ) {
-  val contentColor = if (isToggled) {
-    TwineTheme.colorScheme.brand
-  } else {
-    TwineTheme
-      .colorScheme
-      .primary
-      .copy(
-        alpha = TwineTheme.opacity.hovered
-      )
-  }
-
   val onContentColor = if (isToggled) {
     TwineTheme.colorScheme.onBrand
   } else {
     TwineTheme.colorScheme.onPrimaryContainer
-  }
-
-  val iconResource = if (isToggled) {
-    R.drawable.ic_alert_on
-  } else {
-    R.drawable.ic_alert_off
   }
 
   val contentDescription = if (isToggled) {
@@ -116,16 +133,49 @@ private fun Content(
     R.string.cd_alert_toggle_off
   }
 
+  val alertToggleIcon = AnimatedImageVector
+    .animatedVectorResource(id = R.drawable.avd_alert_toggle)
+
   Box(
-    modifier = modifier
-      .background(
-        color = contentColor,
-        shape = TwineTheme.shapes.medium
-      )
-      .padding(AlertToggleDefaults.contentPadding)
+    modifier = modifier.size(InnerContainerSize),
+    contentAlignment = Alignment.Center,
   ) {
-    val alertToggleIcon = AnimatedImageVector
-      .animatedVectorResource(id = R.drawable.avd_alert_toggle)
+    AnimatedVisibility(
+      visible = isToggled,
+      enter = scaleIn(
+        animationSpec = spring(
+          dampingRatio = TwineSpring.DampingRatioHigh,
+          stiffness = TwineSpring.StiffnessMedium
+        ),
+        initialScale = 0.5f
+      ) + fadeIn(
+        animationSpec = spring(
+          dampingRatio = Spring.DampingRatioNoBouncy,
+          stiffness = TwineSpring.StiffnessMedium
+        ),
+      ),
+      exit = scaleOut(
+        animationSpec = spring(
+          dampingRatio = Spring.DampingRatioNoBouncy,
+          stiffness = TwineSpring.StiffnessMedium
+        ),
+        targetScale = 0.5f
+      ) + fadeOut(
+        animationSpec = spring(
+          dampingRatio = Spring.DampingRatioNoBouncy,
+          stiffness = Spring.StiffnessMedium
+        )
+      ),
+    ) {
+      Box(
+        modifier = Modifier
+          .fillMaxSize()
+          .background(
+            color = TwineTheme.colorScheme.brand,
+            shape = TwineTheme.shapes.large
+          )
+      )
+    }
 
     Icon(
       painter = rememberAnimatedVectorPainter(
