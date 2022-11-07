4c7d74030a0eb986d7ba393abe14f09e2a647c82
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-11-07 13:40:31 +0530

    Remove auto paste option from hero input

--- ui-home/src/main/java/dev/sasikanth/twine/home/HeroInput.kt
+++ ui-home/src/main/java/dev/sasikanth/twine/home/HeroInput.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import dev.sasikanth.twine.common.ui.components.CheckBox
 import dev.sasikanth.twine.common.ui.components.InputField
 import dev.sasikanth.twine.common.ui.components.SubHeader
 import dev.sasikanth.twine.common.ui.components.TwineButton
@@ -20,10 +19,8 @@ import dev.sasikanth.twine.common.ui.theme.TwineTheme
 internal fun HeroInput(
   modifier: Modifier = Modifier,
   text: String,
-  autoPasteEnabled: Boolean,
   onPasteClick: () -> Unit,
   onTextChange: (String) -> Unit,
-  onAutoPasteChange: (Boolean) -> Unit,
 ) {
   Column(
     modifier = modifier
@@ -52,17 +49,11 @@ internal fun HeroInput(
       }
     )
 
-    CheckBox(
-      modifier = Modifier.fillMaxWidth(),
-      checked = autoPasteEnabled,
-      onValueChange = onAutoPasteChange,
-      label = stringResource(id = R.string.home_hero_input_auto_paste_label)
-    )
-
     Text(
       modifier = Modifier
         .fillMaxWidth()
-        .padding(horizontal = 16.dp),
+        .padding(horizontal = 16.dp)
+        .padding(top = 16.dp),
       text = stringResource(id = R.string.home_hero_input_tooltip),
       style = TwineTheme.typography.bodySmall,
       color = TwineTheme.colorScheme.onSurfaceVariant
@@ -77,11 +68,8 @@ private fun HeroInputPreview() {
     Surface {
       HeroInput(
         text = "",
-        autoPasteEnabled = true,
-        onPasteClick = {},
-        onTextChange = {},
-        onAutoPasteChange = {}
-      )
+        onPasteClick = {}
+      ) {}
     }
   }
 }
