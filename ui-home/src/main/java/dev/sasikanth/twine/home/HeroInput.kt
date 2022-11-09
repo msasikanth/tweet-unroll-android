3e8a91820aa537425fbef421350f67d5e89fd45d
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-11-09 07:36:29 +0530

    Set keyboard type to URI in hero input

--- ui-home/src/main/java/dev/sasikanth/twine/home/HeroInput.kt
+++ ui-home/src/main/java/dev/sasikanth/twine/home/HeroInput.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.sasikanth.twine.common.ui.components.IconButton
@@ -62,7 +63,8 @@ internal fun HeroInput(
         onGo = { onGoClick.invoke() }
       ),
       keyboardOptions = KeyboardOptions(
-        imeAction = ImeAction.Go
+        imeAction = ImeAction.Go,
+        keyboardType = KeyboardType.Uri
       )
     )
 
