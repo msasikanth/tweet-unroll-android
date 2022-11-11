23ff2a57d769c898613ecc2ca8c43a06b208d92f
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-11-11 08:22:06 +0530

    Replace clock module with `TestClockModule` in tests

--- common-testing/src/main/java/dev/sasikanth/twine/common/testing/di/TestClockModule.kt
+++ common-testing/src/main/java/dev/sasikanth/twine/common/testing/di/TestClockModule.kt
@@ -2,13 +2,18 @@ package dev.sasikanth.twine.common.testing.di
 
 import dagger.Module
 import dagger.Provides
-import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dagger.hilt.testing.TestInstallIn
+import dev.sasikanth.twine.common.di.modules.ClockModule
 import dev.sasikanth.twine.common.testing.util.TestUserClock
+import dev.sasikanth.twine.common.utils.UserClock
 import javax.inject.Singleton
 
 @Module
-@InstallIn(SingletonComponent::class)
+@TestInstallIn(
+  replaces = [ClockModule::class],
+  components = [SingletonComponent::class]
+)
 object TestClockModule {
 
   @Provides
@@ -16,4 +21,9 @@ object TestClockModule {
   fun providesTestUserClock(): TestUserClock {
     return TestUserClock()
   }
+
+  @Provides
+  fun providesUserClock(
+    testUserClock: TestUserClock
+  ): UserClock = testUserClock
 }
