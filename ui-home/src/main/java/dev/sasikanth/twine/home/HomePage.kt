540902b720bd8e4288eadd0aaad244c17b91fbb1
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-11-16 14:04:12 +0530

    Show conversation syncing items in home page

--- ui-home/src/main/java/dev/sasikanth/twine/home/HomePage.kt
+++ ui-home/src/main/java/dev/sasikanth/twine/home/HomePage.kt
@@ -1,10 +1,15 @@
 package dev.sasikanth.twine.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -18,8 +23,12 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.ExperimentalLifecycleComposeApi
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.sasikanth.twine.common.ui.components.AppBarActionButton
+import dev.sasikanth.twine.common.ui.components.SubHeader
 import dev.sasikanth.twine.common.ui.components.TopAppBar
 import dev.sasikanth.twine.common.ui.theme.TwineTheme
+import dev.sasikanth.twine.data.sync.Status.Enqueued
+import dev.sasikanth.twine.data.sync.Status.Failure
+import dev.sasikanth.twine.data.sync.Status.InProgress
 import dev.sasikanth.twine.common.ui.R as commonR
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalLifecycleComposeApi::class)
@@ -66,6 +75,36 @@ fun HomePage(
         onClearTextClick = viewModel::clearUrl,
         onGoClick = viewModel::validateAndSync
       )
+
+      val syncQueueItems = homeUiState.syncQueue
+
+      LazyColumn(
+        contentPadding = PaddingValues(
+          horizontal = 24.dp,
+          vertical = 24.dp
+        ),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+      ) {
+        item {
+          SubHeader(text = stringResource(id = R.string.home_recent_conversations_sub_head))
+        }
+
+        items(
+          items = syncQueueItems,
+          key = { item -> item.tweetId }
+        ) { item ->
+          when (item.status) {
+            Enqueued,
+            InProgress -> ConversationSyncingListItem(
+              item = item,
+              onCancelClick = viewModel::cancelSync
+            )
+
+            // TODO: Replace with failure item composable
+            Failure -> Box {}
+          }
+        }
+      }
     }
   }
 }
