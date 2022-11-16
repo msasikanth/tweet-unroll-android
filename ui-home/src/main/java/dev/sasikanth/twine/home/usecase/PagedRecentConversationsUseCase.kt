5afa8f93062fe39cdba8a1bae43592086f71e490
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-11-16 20:37:52 +0530

    Add `PagedRecentConversationsUseCase`

---
+++ ui-home/src/main/java/dev/sasikanth/twine/home/usecase/PagedRecentConversationsUseCase.kt
@@ -0,0 +1,28 @@
+package dev.sasikanth.twine.home.usecase
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import dev.sasikanth.twine.data.database.entities.RecentConversation
+import dev.sasikanth.twine.data.database.repository.TweetsRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class PagedRecentConversationsUseCase @Inject constructor(
+  private val tweetsRepository: TweetsRepository
+) {
+
+  companion object {
+    private val PAGING_CONFIG = PagingConfig(
+      pageSize = 50,
+      initialLoadSize = 50
+    )
+  }
+
+  operator fun invoke(): Flow<PagingData<RecentConversation>> {
+    return Pager(
+      config = PAGING_CONFIG,
+      pagingSourceFactory = { tweetsRepository.recentConversations() }
+    ).flow
+  }
+}
