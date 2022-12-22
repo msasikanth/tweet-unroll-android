0efc7cedc1b44dae15b85e94fbdb3397e208cf66
Sasikanth Miriyampalli <hello@sasikanth.dev>
2022-12-22 21:08:35 +0530

    [WIP]

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 @file:Suppress("UnstableApiUsage", "DSL_SCOPE_VIOLATION")
 
+import com.android.build.gradle.LibraryExtension
+import com.android.build.gradle.LibraryPlugin
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
@@ -43,6 +45,22 @@ subprojects {
           "plugin:androidx.compose.compiler.plugins.kotlin:metricsDestination=" +
             project.buildDir.absolutePath + "/compose_metrics"
         )
+      }
+    }
+  }
+
+  plugins.withType<LibraryPlugin>().configureEach {
+    extensions.configure<LibraryExtension> {
+      testOptions {
+        managedDevices {
+          devices {
+            maybeCreate<com.android.build.api.dsl.ManagedVirtualDevice>("pixelApi30").apply {
+              device = "Pixel"
+              apiLevel = 30
+              systemImageSource = "aosp"
+            }
+          }
+        }
       }
     }
   }
